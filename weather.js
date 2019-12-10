b14a477bb2001e4f44d94a2faf9b4164fae678c6
James Dinh <mrjames8124@gmail.com>
2019-12-10 11:12:31 -0800

    background pictures added

--- weather.js
+++ weather.js
@@ -56,27 +56,35 @@ class Weather{
   //   }
   // }
   cityBackground(){
+    //LA STAPLE CENTER
     if (this.lat === "34.052235" && this.lon === "-118.2445") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
-    } else if (this.lat === "40.7306" && this.lon === " -73.9867") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
-    } else if (this.lat === "35.6828" && this.lon === "139.759"){
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
-    }
+      $(".gameContainer").css("background-image", 'url(' + "assets/STAPLESCenternight.jpg" + ')').css("background-size", "contain");
+    }  //NYC Rockerfeller
+    else if (this.lat === "40.7306" && this.lon === " -73.9867") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/newYork.jpg" + ')').css("background-size", "contain");
+    } // Tokyo
+     else if (this.lat === "35.6828" && this.lon === "139.759"){
+      $(".gameContainer").css("background-image", 'url(' + "assets/tokyo.jpg" + ')').css("background-size", "contain");
+    } //SF Bridge
     else if (this.lat === "37.7793" && this.lon === "-122.4193") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "cover");
-    } else if (this.lat === "48.8566" && this.lon === "2.3515"){
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
-    } else if (this.lat === "-26.2051" && this.lon === "28.0497") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
-    } else if (this.lat === "-28.6681" && this.lon === "-49.3384") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+      $(".gameContainer").css("background-image", 'url(' + "assets/GoldenGateBridge-001.jpg" + ')').css("background-size", "cover");
+    } // Paris - eiffel Tower
+    else if (this.lat === "48.8566" && this.lon === "2.3515"){
+      //Johannesburg
+      $(".gameContainer").css("background-image", 'url(' + "assets/paris.jpg " + ')')
+    } //Brazil Igazu Falls
+     else if (this.lat === "-26.2051" && this.lon === "28.0497") {
+     //South Korea
+      $(".gameContainer").css("background-image", 'url(' + "assets/johan.jpg " + ')')
+    } //London
+    else if (this.lat === "-28.6681" && this.lon === "-49.3384") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/iguazu.jpg" + ')')
     } else if (this.lat === "37.5667" && this.lon === "126.9783") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+      $(".gameContainer").css("background-image", 'url(' + "assets/seoul.jpg" + ')')
     } else if (this.lat === "51.5073" && this.lon === "-0.1277") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+      $(".gameContainer").css("background-image", 'url(' + "assets/London.jpg" + ')')
     } else if (this.lat === "37.7006" && this.lon === "-83.9739") {
-      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+      $(".gameContainer").css("background-image", 'url(' + "assets/irvine.jpg" + ')')
     }
   }
 }
