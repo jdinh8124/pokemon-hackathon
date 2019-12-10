33a6269f2451d6b8fd33cd5652894eff86cc5931
James Dinh <mrjames8124@gmail.com>
2019-12-10 11:04:27 -0800

    background pictures added

--- weather.js
+++ weather.js
@@ -36,26 +36,47 @@ class Weather{
     console.log(response);
     this.wind = response.wind.speed;
     this.weather = response.weather[0].main;
-    this.backgroundImages()
+    this.weatherModal()
     // clouds, rain, clear
   }
 
   renderWeatherFail(response){
     console.log(response.responseText);
   }
+  // weatherModal() {
+  //   if (this.weather === "Clouds") {
 
-  backgroundImages() {
-
-    debugger;
-    if (this.weather === "clouds") {
-      $("body").css("background-image", "./assets/173154.jpg")
-    } else if (this.weather === "rain") {
-      $("body").css("background-image", "./assets/173154.jpg")
-    } else {
-      $("body").css("background-image", "assets/173154.jpg")
-      console.log(this.weather);
+  //   } else if (this.weather === "Rain") {
+  //     $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
+  //   } else if (this.weather === "Snow"){
+  //     $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
+  //   }
+  //   else {
+  //     $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "cover");
+  //   }
+  // }
+  cityBackground(){
+    if (this.lat === "34.052235" && this.lon === "-118.2445") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
+    } else if (this.lat === "40.7306" && this.lon === " -73.9867") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
+    } else if (this.lat === "35.6828" && this.lon === "139.759"){
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "contain");
+    }
+    else if (this.lat === "37.7793" && this.lon === "-122.4193") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')').css("background-size", "cover");
+    } else if (this.lat === "48.8566" && this.lon === "2.3515"){
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+    } else if (this.lat === "-26.2051" && this.lon === "28.0497") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+    } else if (this.lat === "-28.6681" && this.lon === "-49.3384") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+    } else if (this.lat === "37.5667" && this.lon === "126.9783") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+    } else if (this.lat === "51.5073" && this.lon === "-0.1277") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
+    } else if (this.lat === "37.7006" && this.lon === "-83.9739") {
+      $(".gameContainer").css("background-image", 'url(' + "assets/173154.jpg" + ')')
     }
   }
-
-
 }
