13ebbacbc23e3c6a9c6b68cbb45ad851ee69c6b1
David Hong <daviddhongg@gmail.com>
2019-12-10 10:26:14 -0800

    completed taco video

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ function initializeApp(){
   var game = new GameBoard();
   game.gameSetup();
 }
+
+function startTacoVideo(){
+  $("iframe").attr('src', "https://www.youtube.com/embed/npjF032TDDQ?&autoplay=1")
+}
